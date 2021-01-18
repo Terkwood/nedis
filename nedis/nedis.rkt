6f9dcd902de191530046888413c235c74bcaa558
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-18 16:16:26 -0500

    fmtp

--- nedis/nedis.rkt
+++ nedis/nedis.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
 
-(provide (except-out (all-from-out racket/base)
-                        #%module-begin)
-   (rename-out [module-begin #%module-begin]))
+(provide (except-out 
+           (all-from-out racket/base)
+            #%module-begin)
+(rename-out [module-begin #%module-begin]))
