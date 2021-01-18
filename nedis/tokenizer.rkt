0e5d26314b138adf9f1778d9bec0b122e62053b9
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-18 18:03:18 -0500

    expand function

--- nedis/tokenizer.rkt
+++ nedis/tokenizer.rkt
@@ -1,9 +1,9 @@
 #lang br
-; e.g. https://beautifulracket.com/basic/the-tokenizer.html
 (require "lexer.rkt" brag/support)
 
-(define (make-tokenizer ip)
+(define (make-tokenizer ip [path #f]) ; optional path argument
   (port-count-lines! ip)
+  (lexer-file-path path)
   (define (next-token) (nedis-lexer ip))
   next-token)
 
