bc62e4cde2c0ff1671aa1db208e7f651036bf2c6
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-18 14:13:40 -0500

    check for commands

--- nedis/lexer-test.rkt
+++ nedis/lexer-test.rkt
@@ -11,5 +11,9 @@
                      (srcloc 'string 1 0 1 1))))
 (check-equal?
  (lex "get")
- (list (srcloc-token (token "get" "get")
+ (list (srcloc-token (token "GET" "get")
+                     (srcloc 'string 1 0 1 3))))
+(check-equal?
+ (lex "GET")
+ (list (srcloc-token (token "GET" "GET")
                      (srcloc 'string 1 0 1 3))))
--- nedis/lexer.rkt
+++ nedis/lexer.rkt
@@ -1,11 +1,26 @@
 #lang br
 ; e.g. https://beautifulracket.com/basic/the-lexer.html
+;      https://github.com/racket/syntax-color/blob/master/syntax-color-lib/syntax-color/racket-lexer.rkt
 (require brag/support)
+(define-lex-abbrevs 
+    [d (char-set "dD")]
+    [e (char-set "eE")]
+    [g (char-set "gG")]
+    [l (char-set "lL")]
+    [s (char-set "sS")]
+    [t (char-set "tT")]
+    
+    [get-cmd (:: g e t )]
+    [set-cmd (:: s e t )]
+    [del-cmd (:: d e l )]
+    )
 (define nedis-lexer
     (lexer-srcloc 
-        ["\n" (token 'NEWLINE lexeme)]
+        ["\n"       (token 'NEWLINE lexeme)]
         [whitespace (token lexeme #:skip? #t)]
-        [(:or "get" "set" "del") (token lexeme lexeme)]
+        [get-cmd    (token 'GET lexeme)]
+        [set-cmd    (token 'SET lexeme)]
+        [del-cmd    (token 'DEL lexeme)]
         [any-string (token 'STRING lexeme)]
     ))
 
