90b65f959e7c0072cb23e51fe06b93e3368fa915
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-18 18:25:11 -0500

    parse program

--- nedis/lexer.rkt
+++ nedis/lexer.rkt
@@ -1,6 +1,9 @@
 #lang br
 ; see https://beautifulracket.com/basic/the-lexer.html
 ; see https://github.com/racket/syntax-color/blob/master/syntax-color-lib/syntax-color/racket-lexer.rkt
+; see http://matt.might.net/articles/lexers-in-racket/
+; see https://gist.github.com/danking/1068185/b93e3bfc77ccfe58a35179c8502d834c0016adcf
+; see https://stackoverflow.com/questions/40194249/in-racket-how-do-i-set-a-lexer-to-recognize-an-identifier-with-a-combination-of
 (require brag/support)
 (define-lex-abbrevs 
     [d (char-set "dD")]
@@ -22,11 +25,11 @@
         [get-cmd    (token 'GET lexeme)]
         [set-cmd    (token 'SET lexeme)]
         [del-cmd    (token 'DEL lexeme)]
+        [(repetition 1 +inf.0 (union (char-range #\a #\z) (char-range #\A #\Z) (char-range #\0 #\9))) (token 'STRING lexeme)]
         [(:or (from/to "\"" "\"") (from/to "'" "'"))
                     (token 'STRING
                         (substring lexeme
                             1 (sub1 (string-length lexeme))))]
-        [any-string (token 'STRING lexeme)]
     ))
 
 (provide nedis-lexer)
