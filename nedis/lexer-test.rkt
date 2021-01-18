437c7f77cbcb692c6270d325f16a5ce36fe40af1
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-18 13:20:37 -0500

    write test

--- nedis/lexer-test.rkt
+++ nedis/lexer-test.rkt
@@ -2,10 +2,6 @@
 (require "lexer.rkt" brag/support rackunit)
 
 (define (lex str)
-  (apply-port-proc basic-lexer str))
-
-(define (lex str)
-  (apply-port-proc basic-lexer str))
+  (apply-port-proc nedis-lexer str))
 
 (check-equal? (lex "") empty)
-(error "todo")
--- nedis/lexer.rkt
+++ nedis/lexer.rkt
@@ -2,5 +2,6 @@
 ; e.g. https://beautifulracket.com/basic/the-lexer.html
 (require brag/support)
 (define nedis-lexer
-    (lexer-srcloc ...))
+    (lexer-srcloc 
+        ["\n" (token 'NEWLINE lexeme)]))
 (provide nedis-lexer)
