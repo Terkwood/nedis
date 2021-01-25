10bd3afed526aeec8040904f076ca4129795e7d7
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-25 17:00:28 -0500

    flail

--- nedis/reader.rkt
+++ nedis/reader.rkt
@@ -3,7 +3,7 @@
 
 (define (read-syntax path port)
   (define parse-tree (parse path (make-tokenizer port)))
-  (define module-datum `(module nedis-module nedis/expander
+  (define module-datum `(module nedis-module "expander.rkt"
                           ,parse-tree))
   (datum->syntax #f module-datum))
 (provide read-syntax)
