b058c4e54e73bd58d84423b992375d6bd5e7614c
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-25 17:04:45 -0500

    hack up defs

--- nedis/expander.rkt
+++ nedis/expander.rkt
@@ -3,7 +3,20 @@
 (define-macro (nedis-module-begin PARSE-TREE)
   #'(#%module-begin
      PARSE-TREE 
-     ··· 
      (display "hello")
      ))
 (provide (rename-out [nedis-module-begin #%module-begin]))
+
+(define (program args) (display args))
+
+(provide program)
+
+(define (instruction token-type arg) (display token-type arg))
+
+(provide instruction)
+
+(define (set token-type key val) (display token-type key val))
+(define (get token-type key) (display token-type key))
+(define (del token-type key) (display token-type key))
+
+(provide set get del)
