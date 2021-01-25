abfbd111532c690ba08bb49e2acfc0732ebf07df
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-25 17:49:35 -0500

    trim

--- nedis/expander.rkt
+++ nedis/expander.rkt
@@ -1,15 +1,13 @@
 #lang br/quicklang
 
-(define-macro (nedis-module-begin PARSE-TREE)
-  #'(#%module-begin
-     PARSE-TREE 
-     (display "hello")
-     ))
+(define-macro 
+  (nedis-module-begin PARSE-TREE)
+  #'(#%module-begin PARSE-TREE ))
 (provide (rename-out [nedis-module-begin #%module-begin]))
 
 (define program (lambda (arg . rest) (display (list arg))))
 
-(define (instruction   arg) (display  arg))
+(define (instruction arg) (display arg))
 
 (define (set token-type key val) (display (list token-type key val)))
 (define (get token-type key) (display (list token-type key)))
