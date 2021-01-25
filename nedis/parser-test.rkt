424ca854c43edcb74463fc19f945dfb41a7f4eb3
Terkwood <38859656+Terkwood@users.noreply.github.com>
2021-01-25 14:18:46 -0500

    extend test

--- nedis/parser-test.rkt
+++ nedis/parser-test.rkt
@@ -4,11 +4,27 @@
 (require nedis/parser nedis/tokenizer brag/support)
 
 
-(define str #<<!STOP
+(define str1 #<<!STOP
 SET myname bun
 GET myname
 DEL myname
 !STOP
 )
 
-(parse-to-datum (apply-tokenizer make-tokenizer str))
+(define str2 #<<!STOP
+SET myname "bun bing"
+GET myname
+DEL myname
+!STOP
+)
+
+(define str3 #<<!STOP
+set "my name" 'bun bing'
+get 'my name'
+del "my name"
+!STOP
+)
+
+(parse-to-datum (apply-tokenizer make-tokenizer str1))
+(parse-to-datum (apply-tokenizer make-tokenizer str2))
+(parse-to-datum (apply-tokenizer make-tokenizer str3))
